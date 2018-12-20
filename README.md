f90128a0101ba8401fee071b3ab7e221c5570032
piatek29535 <piatek29535@student.pwsztar.edu.pl>
2018-12-20 15:17:06 +0100

    MiG Layout coś nie działa

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ReversedBinaryPinCipher
+
+Aplikacja odpowiadająca za zakodowanie numeru PIN pprzez wykorzystanie algorytmu odwrócenia liczby binarnej
--- src/GuiFormApplication.java
+++ src/GuiFormApplication.java
@@ -1,7 +1,9 @@
+import net.miginfocom.swing.MigLayout;
+import org.jdesktop.xswingx.PromptSupport;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuiFormApplication extends JFrame{
 
@@ -9,16 +11,45 @@ public class GuiFormApplication extends JFrame{
     Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 
     //Components
-    
+    JPanel jPanel = new JPanel();
+    JButton jButton = new JButton("Wykonaj!");
+    JTextField jTextField1 = new JTextField();
+    JTextField jTextField2 = new JTextField();
+
 
     public void generateWindow(){
+    /* Frame config */
+
         frame.setVisible(true);
-        frame.setTitle("Odwrotny koder binarny");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLocation(screen.width/4,screen.height/4);
         frame.setSize(screen.width/2,screen.height/2);
+        frame.add(jPanel);
+        frame.setResizable(false);
 
-    //Adding components
+
+    /* Panel edit */
+        jPanel.setBackground(new Color(204,204,204));
+        jPanel.setLayout(new MigLayout("inset 30"));
+        jPanel.add(jButton,"wrap");
+        jPanel.add(jTextField1,"wrap");
+        jPanel.add(jTextField2,"wrap");
+        PromptSupport.setPrompt("Tutaj się wpisuje",jTextField1);
+        PromptSupport.setPrompt("Tutaj jest rezultat",jTextField2);
+
+
+    /* Adding components */
+
+        // ---> JButton
+
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,jTextField1.getText());
+                jTextField1.setText(null);
+            }
+        });
+
 
     }
 
